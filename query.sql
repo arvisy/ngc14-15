9346b6447f1e6a157330b1ef7d64e2f8aa0a7434
arvisy <arviansyahnur3@gmail.com>
2023-11-03 09:56:11 +0700

    change

---
+++ query.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE voting;
+
+CREATE TABLE kandidat (
+    kandidatId INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(255) NOT NULL,
+    partai VARCHAR(255) NOT NULL,
+    lokasi VARCHAR(255) NOT NULL,
+    grade_current DECIMAL(5, 2) NOT NULL
+);
+
+CREATE TABLE pemilih (
+    pemilihId INT AUTO_INCREMENT PRIMARY KEY,
+    nama_depan VARCHAR(255) NOT NULL,
+    nama_belakang VARCHAR(255) NOT NULL,
+    jenis_kelamin VARCHAR(20) NOT NULL,
+    umur INT NOT NULL
+);
+
+CREATE TABLE pilihan (
+    pilihanId INT AUTO_INCREMENT PRIMARY KEY,
+    kandidatId INT NOT NULL,
+    pemilihId INT NOT NULL,
+    FOREIGN KEY (kandidatId) REFERENCES kandidat(kandidatId),
+    FOREIGN KEY (pemilihId) REFERENCES pemilih(pemilihId)
+);
